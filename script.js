8284e3d92160e775c843ab7b440d435b3d65f84f
Dagmar Lewis <dagmarlewis8@gmail.com>
2022-04-14 16:41:50 -0500

    got data into dom

--- script.js
+++ script.js
@@ -3,5 +3,5 @@
 fetch('https://v6.exchangerate-api.com/v6/1d2ffcada273c97d7dd73c6d/latest/USD')
     .then((response) => response.json())
     .then((data) => {
-        console.log(data)
+        document.querySelector('span').innerText = data.conversion_rates.     JMD
     })
